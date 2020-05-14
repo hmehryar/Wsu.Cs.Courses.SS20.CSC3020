b454f79b49b7d750433add2c52a0f793935f9617
Ryan Kaszubski <gn2289@wayne.edu>
2020-05-14 19:36:02 -0400

    CSC3020: gn2289: Lecture04Source

--- src/com/csc3020/lecture04/gn2289/Lecture04Source01.java
+++ src/com/csc3020/lecture04/gn2289/Lecture04Source01.java
@@ -2,6 +2,6 @@ package com.csc3020.lecture04.gn2289;
 
 public class Lecture04Source01 {
     public static void main(String[] args) {
-        System.out.println("gn2289: Ryan Kaszubski");
+        System.out.println("gn2289: Ryan Kaszubski!");
     }
 }
