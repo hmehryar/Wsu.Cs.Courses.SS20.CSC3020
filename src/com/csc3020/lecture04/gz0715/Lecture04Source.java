a501dbccac120a6a08a68863142094a1dc52a442
Saim Beg <gz0715@wayne.edu>
2020-05-14 19:38:19 -0400

    CSC3020: gz0715: Lecture04Source, gz0715 folders added

--- src/com/csc3020/lecture04/gz0715/Lecture04Source.java
+++ src/com/csc3020/lecture04/gz0715/Lecture04Source.java
@@ -2,6 +2,6 @@ package com.csc3020.lecture04.gz0715;
 
 public class Lecture04Source {
     public static void main(String[] args) {
-        System.out.println("gz0715: Saim Beg");
+        System.out.println("gz0715: Saim Beg.");
     }
 }
