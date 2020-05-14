df06e2d0d03c48596729f2ac0b1fe4f71d1838c8
dylandenha <dylandenha@gmail.com>
2020-05-14 19:49:17 -0400

    CSC 3020: gh9762: Lecture04Source01

--- src/com/csc3020/lecture04/gh9762/Lecture04Source01.java
+++ src/com/csc3020/lecture04/gh9762/Lecture04Source01.java
@@ -2,6 +2,6 @@ package com.csc3020.lecture04.gh9762;
 
 public class Lecture04Source01 {
     public static void main(String[] args) {
-        System.out.println(" gh9762:Dylan Denha");
+        System.out.println(" gh9762:Dylan  Denha");
     }
 }
