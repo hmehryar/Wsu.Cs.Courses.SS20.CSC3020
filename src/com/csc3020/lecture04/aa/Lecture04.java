1174c71ae53f25cb9be4dac51540820bfbc35b3f
Anasebenmalek Abdulkareem <ga9560@wayne.edu>
2020-05-14 19:44:43 -0400

    CSC3020: ga9560: Lecture04Source

---
+++ src/com/csc3020/lecture04/aa/Lecture04.java
@@ -0,0 +1,8 @@
+package com.csc3020.lecture04.aa;
+
+public class Lecture04 {
+    public static void main(String[] args) {
+        System.out.println("ga9560: Anasebenmalek Abdulkareem");
+        System.out.println("Test");
+    }
+}
