a4a1eeaff3ff013245c42efb5ca3a8c7b35811ac
Saim Beg <gz0715@wayne.edu>
2020-05-19 20:38:12 -0400

    CSC3020: gz0715: Lecture05Source, Flight, CalculateEngine

--- src/com/csc3020/Lecture05/gz0715/Flight.java
+++ src/com/csc3020/Lecture05/gz0715/Flight.java
@@ -1,4 +1,30 @@
 package com.csc3020.lecture05.gz0715;
-
+// Lecture 05: Class
 public class Flight {
+    private int passengers;
+    private int seats;
+
+    public Flight() {
+        seats = 150;
+        passengers = 0;
+    }
+
+    void add1Passenger() {
+        if (passengers < seats)
+            passengers += 1;
+        else
+            handleTooMany();
+    }
+
+    private void handleTooMany() {
+        System.out.println("Too many!");
+    }
+
+    public boolean hasRoom(Flight f2) {
+        int total = passengers + f2.passengers;
+        if (total <= seats)
+            return true;
+        else
+            return false;
+    }
 }
