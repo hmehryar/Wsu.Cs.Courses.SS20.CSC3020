975c88f0885feb1aeb8079d3616f451803536c0e
tia-gijo <go1277@wayne.edu>
2020-05-21 19:51:15 -0400

    CSC3020: go1277: Lecture06Source

---
+++ src/com/csc3020/lecture06/go1277/Lecture06Source.java
@@ -0,0 +1,63 @@
+package com.csc3020.lecture06.go1277;
+
+public class Lecture06Source
+{
+    public static class Flight
+    {
+        private int passengers;
+        private int seats;
+        public Flight()
+        {
+            seats = 150;
+            passengers = 0;
+        }
+
+        private void handleTooMany()
+        {
+            System.out.println("Too Many");
+        }
+        public int getSeats()
+        {
+            return seats;
+        }
+        public void setSeats(int seats)
+        {
+            this.seats = seats;
+        }
+        public void add1Passenger()
+        {
+            if(passengers < seats)
+                passengers += 1;
+            else
+                handleTooMany();
+        }
+
+        public boolean hasRoom(Flight f2)
+        {
+            int total = this.passengers + f2.passengers;
+            return total <= seats;
+        }
+        public Flight createNewWithBoth(Flight f2)
+        {
+            Flight newFlight= new Flight();
+            newFlight.seats= seats;
+            newFlight.passengers= passengers + f2.passengers;
+            return newFlight;
+        }
+        Flight lax1 = new Flight();
+        Flight lax2 = new Flight();
+        // add passengers to both flights
+        Flight lax3 = null;
+
+        if(lax1.hasRoom(lax2))
+        {
+            lax3 =lax1.createNewWithBoth(lax2);
+        }
+        // do some other work
+        if(lax3 != null )
+        {
+            System.out.println("Flights combined");
+        }
+
+    }
+}
