b0caa83b5db89f278301a60d8006dfa6fe99b593
Kevin Kluka <Kevinmkluka@gmail.com>
2020-05-22 11:44:18 -0400

    AV7071 Update Lecture06

--- src/com/csc3020/lecture06/av7071/Lecture06Source.java
+++ src/com/csc3020/lecture06/av7071/Lecture06Source.java
@@ -1,62 +1,68 @@
 package com.csc3020.lecture06.av7071;
+/**
+ * Author: Kevin Kluka
+ */
+public class Lecture06Source {
+    public static void main(String[] args) {
+        Flight oFlight1 = new Flight();
+        for (int count = 0; count < 20; count++) {
+            oFlight1.add1Passenger();
+        }
+        Flight oFlight2 = new Flight();
+        for (int count = 0; count < 30; count++) {
+            oFlight1.add1Passenger();
+        }
+        boolean flight1HasRoom = oFlight1.hasRoom(oFlight2);
+        System.out.println(flight1HasRoom);
 
-public class Lecture06Source
-{
-    public static class Flight
-    {
-        private int passengers;
-        private int seats;
-        public Flight()
-        {
-            seats = 150;
-            passengers = 0;
+        oFlight2 = new Flight();
+        for (int count = 0; count < 131; count++) {
+            oFlight2.add1Passenger();
         }
+        flight1HasRoom = oFlight1.hasRoom(oFlight2);
+        System.out.println(flight1HasRoom);
 
-        private void handleTooMany()
-        {
-            System.out.println("Too Many");
-        }
-        public int getSeats()
-        {
-            return seats;
-        }
-        public void setSeats(int seats)
-        {
-            this.seats = seats;
-        }
-        public void add1Passenger()
-        {
-            if(passengers < seats)
-                passengers += 1;
-            else
-                handleTooMany();
-        }
 
-        public boolean hasRoom(Flight f2)
-        {
-            int total = passengers + f2.passengers;
-            return total <= seats;
-        }
-        public Flight createNewWithBoth(Flight f2)
-        {
-            Flight newFlight= new Flight();
-            newFlight.seats= seats;
-            newFlight.passengers= passengers + f2.passengers;
-            return newFlight;
-        }
+        oFlight2 = new Flight();
+        oFlight2.add1Passenger();
+        oFlight1.print();
+        Flight oMergedFlight = oFlight1.createFlightWithBoth(oFlight2);
+        oFlight1.print();
+        oMergedFlight.print();
+
         Flight lax1 = new Flight();
         Flight lax2 = new Flight();
-        // add passengers to both flights
-        Flight lax3 = null;
 
-        if(lax1.hasRoom(lax2))
-        {
-            lax3 =lax1.createNewWithBoth(lax2);
+        //Adding Passengers
+        for (int count = 0; count < 20; count++) {
+            lax1.add1Passenger();
         }
-        // do some other work
-        if(lax3 != null )
-        {
-            System.out.println("Flights combined");
+//        for (int count = 0; count < 30; count++) {
+//            lax2.add1Passenger();
+//        }
+        for (int count = 0; count < 131; count++) {
+            lax2.add1Passenger();
         }
+        lax1.print();
+        lax2.print();
+        Flight lax3 = null;
+//        Flight lax3 = new Flight();
+
+        if(lax1.hasRoom(lax2)) {
+            lax3 = lax1.createFlightWithBoth(lax2);
+        }
+
+        if(lax3 != null) {
+            lax3.print();
+        }
+        else {
+            System.out.println("The flights haven't been merged!!");
+        }
+
+        //Accessors and Mutators
+        Flight slcToNYC = new Flight();
+        slcToNYC.setSeats(250);
+        System.out.println(slcToNYC.getSeats());
+
     }
 }
