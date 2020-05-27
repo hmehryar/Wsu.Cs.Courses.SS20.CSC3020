6fbf363ea4faab6ec593dadd658f0a3c5dd46df8
Saim Beg <gz0715@wayne.edu>
2020-05-26 20:19:34 -0400

    CSC3020: gz0715: HW02Source (No Code), Lecture07Source

---
+++ src/com/csc3020/lecture07/gz0715/Lecture07Source.java
@@ -0,0 +1,35 @@
+package com.csc3020.lecture07.gz0715;
+
+public class Lecture07Source {
+    public static void main(String[] args) {
+        Earth oEarth = new Earth();
+        System.out.println(oEarth.circumferenceInMiles);
+        System.out.println(oEarth.circumferenceInKilometers);
+
+        Flight oFlight = new Flight();
+        // Object obj = new Object();
+
+        Passenger bob = new Passenger();
+        System.out.println(bob.getFreeBags());
+        bob.setFreeBags(4);
+        System.out.println(bob.getFreeBags());
+
+        Passenger arsi = new Passenger(3);
+        System.out.println(arsi.getFreeBags());
+
+        Passenger jane = new Passenger(4);
+        jane.setCheckBags(3);
+        System.out.println(jane.getFreeBags() + ", " + jane.getCheckBags());
+
+        jane = new Passenger(4, 3);
+        System.out.println(jane.getFreeBags() + ", " + jane.getCheckBags());
+
+//        Passenger cheapJoe = new Passenger(.01d); // perBagFee Constructor was set to private
+//        cheapJoe.setFreeBags(4);
+//        cheapJoe.setCheckBags(7);
+//        System.out.println(cheapJoe.getFreeBags() + ", " + cheapJoe.getCheckBags() + ", " + cheapJoe.getPerBagFee());
+
+        Passenger joe = new Passenger(2);
+        System.out.println(joe.getFreeBags() + ", " + joe.getCheckBags() + ", " + joe.getPerBagFee());
+    }
+}
