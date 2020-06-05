4ddf1ff5c282e0bdf5f53cd635b99405a0b58cb1
jbvinsonWSU <hc7391@wayne.edu>
2020-06-04 21:13:58 -0400

    CSC 3020: hc7391: Lecture10Source

---
+++ src/com/csc3020/lecture10/hc7391/CargoOnlyPilot.java
@@ -0,0 +1,9 @@
+package com.csc3020.lecture10.hc7391;
+
+public class CargoOnlyPilot extends Pilot{
+    @Override // must override method from abstract class
+    public boolean canAccept(Flight flight) {
+       return flight.getPassengers() ==0;
+        //cargo flight is passengers are zero
+    }
+}
