eb84b1661d66317b8e612f52ae9143bbf124b987
MazenZeidan <57071949+MazenZeidan@users.noreply.github.com>
2020-06-04 20:31:30 -0400

    AdultPassenger.java:go5334 CargoFlight.java: go5334 CargoFlightType1.java: go5334 CargoOnlyPilot.java: go5334 Flight.java: go5334 FullLicensePilot.java: go5334 Lecture10Source.java: go5334 Passenger.java: go5334 Person.java: go5334 Pilot.java: go5334 Student.java: go5334

---
+++ src/com/csc3020/lecture10/go5334/FullLicensePilot.java
@@ -0,0 +1,13 @@
+package com.csc3020.lecture10.go5334;
+
+public class FullLicensePilot extends Pilot{
+
+    @Override
+
+    public boolean canAccept(Flight flight) {
+
+        return true;
+
+    }
+
+}
---
+++ src/com/csc3020/lecture10/go5334/Student.java
@@ -0,0 +1,5 @@
+package com.csc3020.lecture10.go5334;
+
+final public class Student extends Person{
+
+}
