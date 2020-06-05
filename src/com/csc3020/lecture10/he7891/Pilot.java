f3d8adbc5c0a26fde3fc64ef6e8c4344930fd171
nrtattrie <64509108+nrtattrie@users.noreply.github.com>
2020-06-04 20:14:27 -0400

    CSC3020:he7891:Lecture10

---
+++ src/com/csc3020/lecture10/he7891/Pilot.java
@@ -0,0 +1,25 @@
+package com.csc3020.lecture10.he7891;
+// *
+// Author: Nathan Tattrie
+// Student ID: he7891
+// Lecture10: Class Inheritance
+// *
+
+public abstract class Pilot {
+    private Flight currentFlight;
+    public void fly(Flight flight){
+        if (canAccept(flight)) {
+            currentFlight = flight;
+        }
+        else {
+            handleCantAccept();
+        }
+    }
+
+
+    public abstract boolean canAccept(Flight flight);
+
+    public void handleCantAccept() {
+        System.out.println("Can't accept the flight.");
+    }
+}
---
+++ src/com/csc3020/lecture10/he7891/Student.java
@@ -0,0 +1,10 @@
+package com.csc3020.lecture10.he7891;
+// *
+// Author: Nathan Tattrie
+// Student ID: he7891
+// Lecture10: Class Inheritance
+// *
+
+final public class Student extends Person {
+
+}
