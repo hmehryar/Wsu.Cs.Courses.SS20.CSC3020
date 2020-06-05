8bb4f40277dbab74553a08a46a61b759c3d4957d
Ryan Kaszubski <gn2289@wayne.edu>
2020-06-04 20:20:11 -0400

    Cs3020: gn2289: Lecture10Source

---
+++ src/com/csc3020/lecture10/gn2289/CargoFlight.java
@@ -0,0 +1,52 @@
+package com.csc3020.lecture10.gn2289;
+
+public class CargoFlight extends Flight {
+    public float usedCargoSpace;
+    public float maxCargoSpace = 1000.0f;
+    public int seats = 12;
+    public CargoFlight(){
+        //super();
+    }
+    public CargoFlight(int flightNumber) {
+        super(flightNumber);
+    }
+
+    public CargoFlight(int flightNumber, float maxCargoSpace) {
+        this(flightNumber);
+        this.maxCargoSpace = maxCargoSpace;
+    }
+
+    public CargoFlight(float maxCargoSpace) {
+        this.maxCargoSpace = maxCargoSpace;
+    }
+
+
+    @Override
+    public int getSeats() {
+        return 12;
+    }
+
+    final public void add1Package(float h, float w, float d){
+        double size = h * w * d;
+        if (hasCargoSpace(size)) {
+            usedCargoSpace += size;
+        }else{
+            handleNpSpace();
+        }
+    }
+
+    private void handleNpSpace() {
+        System.out.println("Not enough space");
+    }
+
+    private boolean hasCargoSpace(double size) {
+        boolean result = usedCargoSpace + size <= maxCargoSpace;
+        return result;
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("max cargo space= " +maxCargoSpace+" used cargo space = "+usedCargoSpace);
+    }
+}
