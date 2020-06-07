19596294447c53bcc227dec4f93b1a5ba057b98e
tiagijo <tiagijo@gmail.com>
2020-06-06 23:39:59 -0400

    CSC3020: go1277: Lecture10Source

---
+++ src/com/csc3020/lecture10/go1277/FullLicensePilot.java
@@ -0,0 +1,10 @@
+package lecture10.go1277;
+
+//Lecture 10
+// Author: Raisa Zaman
+public class FullLicensePilot extends Pilot {
+    @Override
+    public boolean canAccept(Flight flight) {
+        return true;
+    }
+}
