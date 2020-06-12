0cd9ba8a6923ab11af19ddecc43a75f72a5c6556
Raisa Zaman <49289864+raisazaman02@users.noreply.github.com>
2020-06-11 22:34:28 -0400

    CSC3020: gn8271: Lecture12Source

---
+++ src/com/csc3020/lecture12/gn8271/Passenger.java
@@ -0,0 +1,56 @@
+package lecture12.gn8271;
+//Lecture 12
+// Author: Raisa Zaman
+public class Passenger {
+    private int checkedBags;
+    private final int freeBags ;
+
+    public int getCheckedBags() {
+        return checkedBags;
+    }
+    public void setCheckedBags(int checkedBags) {
+        this.checkedBags = checkedBags;
+    }
+    public int getFreeBags() {
+        return freeBags;
+    }
+//    public void setFreeBags(int freeBags) {
+//        this.freeBags = freeBags;
+//    }
+
+    private double perBagFee;
+
+    public double getPerBagFee() {
+        return perBagFee;
+    }
+
+    public void setPerBagFee(double perBagFee) {
+        this.perBagFee = perBagFee;
+    }
+    public Passenger(){
+        freeBags = 0;
+    }
+    public Passenger(int freeBags){
+        //this(freeBags>1?25.0d:50.0d);
+//          if(freeBags>1){
+//
+//              perBagFee=25.0d;
+//          }else{
+//              perBagFee=50.0d;
+//          }
+
+        this.freeBags=freeBags;
+
+    }
+    public Passenger(int freeBags, int checkedBags){
+        //this.freeBags=freeBags;
+        this(freeBags);
+        this.checkedBags=checkedBags;
+    }
+    private Passenger(double perBagFee){
+        this();
+        this.perBagFee=perBagFee;
+    }
+}
+
+
