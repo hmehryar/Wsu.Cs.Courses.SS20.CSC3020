88d8f003eb3dfe465deb3c6533eed71d44289651
eu6179 <eu6179@wayne.edu>
2020-06-16 22:06:42 -0400

    Lecture 12/13

---
+++ src/com/csc3020/lecture12/eu6179/Passenger.java
@@ -0,0 +1,44 @@
+package com.csc3020.lecture12.eu6179;
+
+ public class Passenger {
+    private int checkedBags;
+    private final int freeBags ;
+
+    public int getCheckedBags() {
+        return checkedBags;
+    }
+    public void setCheckedBags(int checkedBags) {
+        this.checkedBags = checkedBags;
+    }
+    public int getFreeBags() {
+        return freeBags;
+    }
+
+
+    private double perBagFee;
+
+    public double getPerBagFee() {
+        return perBagFee;
+    }
+
+    public void setPerBagFee(double perBagFee) {
+        this.perBagFee = perBagFee;
+    }
+    public Passenger(){
+        freeBags = 0;
+    }
+    public Passenger(int freeBags){
+
+        this.freeBags=freeBags;
+
+    }
+    public Passenger(int freeBags, int checkedBags){
+        //this.freeBags=freeBags;
+        this(freeBags);
+        this.checkedBags=checkedBags;
+    }
+    private Passenger(double perBagFee){
+        this();
+        this.perBagFee=perBagFee;
+    }
+}
