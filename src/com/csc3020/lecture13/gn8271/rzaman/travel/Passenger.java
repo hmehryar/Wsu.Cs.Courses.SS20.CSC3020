7948e4c59575bef0fda230d2f379ae753ec89e7d
Raisa Zaman <49289864+raisazaman02@users.noreply.github.com>
2020-06-16 23:47:35 -0400

    CSC3020: gn8271: Lecture13Source

---
+++ src/com/csc3020/lecture13/gn8271/rzaman/travel/Passenger.java
@@ -0,0 +1,57 @@
+package lecture13.gn8271.rzaman.travel;
+
+public class Passenger {
+    private int checkedBags;
+    private final int freeBags ;
+    private Flight relateFlight;
+
+    public int getCheckedBags() {
+        return checkedBags;
+    }
+    public void setCheckedBags(int checkedBags) {
+        this.checkedBags = checkedBags;
+    }
+    public int getFreeBags() {
+        return freeBags;
+    }
+//    public void setFreeBags(int freeBags) {
+//        this.freeBags = freeBags;
+//    }
+
+    private double perBagFee;
+
+    public double getPerBagFee() {
+        return perBagFee;
+    }
+
+    public void setPerBagFee(double perBagFee) {
+        this.perBagFee = perBagFee;
+    }
+    public Passenger(){
+        freeBags = 0;
+        relateFlight = new Flight();
+        relateFlight.legacyFlightNumber = 0;
+    }
+    public Passenger(int freeBags){
+        //this(freeBags>1?25.0d:50.0d);
+//          if(freeBags>1){
+//
+//              perBagFee=25.0d;
+//          }else{
+//              perBagFee=50.0d;
+//          }
+
+        this.freeBags=freeBags;
+
+    }
+    public Passenger(int freeBags, int checkedBags){
+        //this.freeBags=freeBags;
+        this(freeBags);
+        this.checkedBags=checkedBags;
+    }
+    private Passenger(double perBagFee){
+        this();
+        this.perBagFee=perBagFee;
+    }
+}
+
