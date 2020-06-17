027bd8f6778a9432191ed7a88bbbfe44c3906cfb
tiagijo <51178555+tiagijo@users.noreply.github.com>
2020-06-16 23:29:17 -0500

    CSC3020: go1277: Lecture13Source

---
+++ src/com/csc3020/lecture13/go1277/Lecture13Source.java
@@ -0,0 +1,26 @@
+package lecture13.go1277;
+
+import lecture12.go1277.CargoFlight;
+
+import lecture13.go1277.gijo.travel.Flight;
+import lecture13.go1277.gijo.travel.Passenger;
+
+
+import lecture13.go1277.xyzcompany.bar.*;
+
+public class Lecture13Source {
+    public static void main(String[] args) {
+        //new lecture13.gn8271.Lecture13Source();
+        Flight oFlight = new Flight();
+        lecture11.gn8271.Flight oLecture11Flight = new lecture11.gn8271.Flight();
+        CargoFlight oCargoFlight = new CargoFlight();
+
+        Flight lax175 = new Flight(175);
+        Beer liteBeer = new Beer ();
+        Passenger jane = new Passenger();
+        Wine merlot = new Wine();
+
+        //Lax175.flightNumber
+        lax175.getFlightNumber();
+    }
+}
