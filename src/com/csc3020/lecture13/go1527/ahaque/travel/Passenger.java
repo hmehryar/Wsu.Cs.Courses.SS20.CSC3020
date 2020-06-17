58bdf5a89a0a13823a8ad76d12a18c5cf5f836f9
Ahtesamul Haque <go1527@wayne.edu>
2020-06-17 00:31:48 -0400

    CSC 3020: go1527: Lecture13Source

---
+++ src/com/csc3020/lecture13/go1527/ahaque/travel/Passenger.java
@@ -0,0 +1,45 @@
+package csc3020.lecture13.go1527.ahaque.travel;
+
+public class Passenger {
+    private int checkedBags;
+    private final int freeBags;
+
+    public int getCheckedBags() {
+        return checkedBags;
+    }
+    public void setCheckedBags(int checkedBags) {
+        this.checkedBags = checkedBags;
+    }
+
+    public int getFreeBags() {
+        return freeBags;
+    }
+//    public void setFreeBags(int freeBags) {
+//        this.freeBags = freeBags;
+//    }
+
+    private double perBagFee;
+
+    public double getPerBagFee() {
+        return perBagFee;
+    }
+    public void setPerBagFee(double perBagFee) {
+        this.perBagFee = perBagFee;
+    }
+
+    public Passenger() {
+        freeBags = 0;
+    }
+    public Passenger(int freeBags) {
+//        this(freeBags > 1 ? 25.0d : 50.0d);
+        this.freeBags = freeBags;
+    }
+    public Passenger(int freeBags, int checkedBags) {
+        this(freeBags);
+        this.checkedBags = checkedBags;
+    }
+    private Passenger(double perBagFee) {
+        this();
+        this.perBagFee = perBagFee;
+    }
+}
