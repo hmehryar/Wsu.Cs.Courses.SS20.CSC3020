6127f54d7522935e32b34da45433502a80b9803a
Ryan Kaszubski <gn2289@wayne.edu>
2020-06-18 21:01:47 -0400

    Cs 3020: gn2289: Lecture14Source

---
+++ src/com/csc3020/lecture14/gn2289/Person.java
@@ -0,0 +1,15 @@
+package csc3020.lecture14.gn2289;
+
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+
+}
