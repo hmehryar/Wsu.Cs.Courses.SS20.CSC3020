85a12a97940bf1f7a251f99633d911257386e2cc
maiselkelly <maiselkelly@gmail.com>
2020-06-18 20:49:23 -0400

    CSC3020: em0608: lecture14source Kelly Maisel

---
+++ src/com/csc3020/lecture14/em0608/CrewMember.java
@@ -0,0 +1,27 @@
+package com.csc3020.lecture14.em0608;
+
+public class CrewMember extends Person {
+    private FlightCrewJob job;
+
+    public CrewMember(FlightCrewJob job) {
+        this.job = job;
+    }
+
+    public void setJob(FlightCrewJob job) {
+        this.job = job;
+    }
+
+    public FlightCrewJob getJob() {
+        return job;
+    }
+
+    @Override
+    public String toString() {
+        return "CrewMember{" +
+                "name" + this.getName() +
+                "job=" + job +
+                '}';
+    }
+
+
+}
