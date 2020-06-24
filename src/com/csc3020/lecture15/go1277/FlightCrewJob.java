2c2f6a6e3b165222678f15b943c01ed7cb3cd46b
tiagijo <51178555+tiagijo@users.noreply.github.com>
2020-06-23 23:37:27 -0400

    CSC3020: go1277: Lecture15Source

---
+++ src/com/csc3020/lecture15/go1277/FlightCrewJob.java
@@ -0,0 +1,8 @@
+package lecture15.go1277;
+
+public enum FlightCrewJob {
+    Pilot,
+    Copilot,
+    FlightAttendant,
+    CoPilot, AirMarshal
+}
---
+++ src/com/csc3020/lecture15/go1277/Person.java
@@ -0,0 +1,21 @@
+package lecture15.go1277;
+
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
