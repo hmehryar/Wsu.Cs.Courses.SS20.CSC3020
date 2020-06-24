b18dc67601cef4167e8c880925aa5c5aef5528a7
Patricia Liu <patzhenliu10@gmail.com>
2020-06-23 20:31:30 -0400

    CSC3020: go5621: Lecture15Source

---
+++ src/com/csc3020/lecture15/go5261/Person.java
@@ -0,0 +1,20 @@
+package com.csc3020.lecture15.go5261;
+
+public class Person {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
