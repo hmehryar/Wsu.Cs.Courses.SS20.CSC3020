5d089b3e76fabc462e1ff8441d3a8e9ef561f764
Manraj23s <gg6997@wayne.edu>
2020-06-25 20:52:21 -0400

    CSC3020: gg6997: Lecture16Source

--- src/com/csc3020/lecture16/gg6997/Lecture16Source.java
+++ src/com/csc3020/lecture16/gg6997/Lecture16Source.java
@@ -4,159 +4,127 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class Lecture16Source {
-    public static void main(String[] args) {
-
-//        // Reading One Byte at a Time
-//        InputStream input = null; // create input stream
+    public static void main(String[] args)  {
+//        InputStream input=null;
+//
+//        //Reading One Byte at a Time
+//        try{
+//            int intVal;
+//            while((intVal= input.read())>=0){
+//                byte byteVal=(byte) intVal;
+//                //To Do: Do something with byteVal
+//            }
+//        }
+//        catch (IOException ioException){
+//            System.out.println("IOException : Not Implemented Exception");
+//        }
+//        Reader reader=null;
+//        //Reading One Character at a Time
 //        try {
 //            int intVal;
-//            while ((intVal = input.read()) >= 0) {
-//                byte byteVal = (byte) intVal;
-//                // do something with byteVal
+//            while((intVal=reader.read())>=0){
+//                char charVal=(char) intVal;
+//                //To Do: Do something with charVal
+//            }
+//        }catch (IOException ioException){
+//            System.out.println("IOException : Not Implemented Exception");
+//        }
+//
+//        //Reading Array of Bytes
+//        try{
+//            int length;
+//            byte[] byteBuff=new byte[10];
+//            while ((length=input.read(byteBuff))>=0){
+//                for (int index = 0; index <length ; index++) {
+//                    byte byteVal=byteBuff[index];
+//                    //To Do: Do something with byteVal
+//                }
 //            }
 //        }
 //        catch (IOException ioException){
 //            System.out.println("IOException : Not Implemented Exception");
 //        }
 //
-//
-//        // Reading Array of Bytes
-//        try {
+//        //Reading Array of Characters
+//        try{
 //            int length;
-//            byte[] byteBuff = new byte[10];
-//            while ((length = input.read(byteBuff)) >= 0) {
-//                for (int i = 0; i < length; ++i) {
-//                    byte byteVal = byteBuff[i];
-//                    // do something with byteVal
+//            char[] charBuff=new char[10];
+//            while( (length= reader.read(charBuff))>=0   ){
+//                for (int index = 0; index < length; index++) {
+//                    char charVal=charBuff[index];
+//                    //To Do: Do something with byteVal
 //                }
 //            }
-//        }
-//        catch (IOException ioException) {
+//        }catch (IOException ioException){
 //            System.out.println("IOException : Not Implemented Exception");
 //        }
 //
-//
-//        // Reading One Character at a Time
-//        Reader reader = null; // create input stream
-//        try {
-//            int intVal;
-//            while ((intVal = reader.read()) >= 0) {
-//                char charVal = (char) intVal;
-//                // do something with charVal
-//            }
-//        }
-//        catch (IOException ioException){
-//            System.out.println("IOException : Not Implemented Exception");
-//        }
-//
-//        // Reading Array of Characters
-//        try {
-//            int length;
-//            char[] charBuff = new char[10];
-//            while ((length = reader.read(charBuff)) >= 0) {
-//                for (int i = 0; i < length; ++i) {
-//                    char charVal = charBuff[i];
-//                    // do something with charVal
-//                }
-//            }
-//        }
-//        catch (IOException ioException) {
-//            System.out.println("IOException : Not Implemented Exception");
-//        }
-//
-//
-//        // Writing Bytes
-//        OutputStream output = null;
-//        try {
-//            // Single Byte
-//            byte byteVal = 100;
+//        //Writing Bytes
+//        OutputStream output=null;
+//        try{
+//            byte byteVal=100;
 //            output.write(byteVal);
 //
-//            // Array of Bytes
-//            byte[] byteBuff = {0, 63, 127};
+//            byte[] byteBuff={0,63,127};
 //            output.write(byteBuff);
-//        }
-//        catch (IOException ioException) {
+//        }catch (IOException ioException){
 //            System.out.println("IOException : Not Implemented Exception");
 //        }
 //
-//
-//        // Writing Characters
-//        Writer writer = null;
-//        try {
-//            // Single Character
-//            char charVal = 'a';
+//        //Writing Characters
+//        Writer writer=null;
+//        try{
+//            char charVal='a';
 //            writer.write(charVal);
 //
-//            // Array of Characters
-//            char[] charBuff = {'a', 'b', 'c'};
+//            char[] charBuff= {'a','b','z'};
 //            writer.write(charBuff);
 //
-//            // String
-//            String stringVal = "Hello, World!";
+//            String stringVal="Hello World";
 //            writer.write(stringVal);
-//        }
-//        catch (IOException ioException) {
+//
+//        }catch (IOException ioException){
 //            System.out.println("IOException : Not Implemented Exception");
 //        }
 //
-//
-//        // Exception Handling and Closing
-//        //Reader reader;
+//        //Exception Handling and Closing
 //        try {
-//            reader = new FileReader("D:\\resource.txt"); //open reader
-//            // do something with reader
-//        }
-//        catch (IOException ioException) {
-//            System.out.println("IOException : Not Implemented Exception");
-//        }
-//        finally {
-//            try {
-//                reader.close();
-//            }
-//            catch (IOException ioException) {
-//                System.out.println("IOException : 2nd Level : Not Implemented Exception");
-//            }
-//        }
+//            reader=new FileReader("d:\\resource.txt");
+//            //do something with reader
 //
-////        interface AutoCloseable {
-////            void close() throws Exception;
-////        }
-////
-////        interface Closeable extends AutoCloseable {
-////            void close throws IOException;
-////        }
-
-
+//        }catch (IOException ioException){
+//            System.out.println("IOException : Not Implemented Exception");
+//        }finally {
+//            try{
+//                reader.close();
+//            }catch(IOException ioException){
+//                System.out.println("IOException : 2nd Level: Not Implemented Exception");
+//            }
+//
+//        }
         doTryCatchFinally();
     }
-
-    public static void doTryCatchFinally() {
-        char[] buffer = new char[8];
+    public static void doTryCatchFinally(){
+        char[] buff=new char[8];
         int length;
-        Reader reader = null;
-        try {
-            reader = lecture16.gg6997.Helper.openReader("file1.txt");
-            while ((length = reader.read(buffer)) >= 0) {
-                System.out.println("\nlength: " + length);
-                for (int i = 0; i < length; i++) {
-                    System.out.print(buffer[i]);
+        Reader reader=null;
+        try{
+            reader=Helper.openReader("file1.txt");
+            while ((length= reader.read(buff))>=0){
+                System.out.println("\nlength: "+length);
+                for (int index = 0; index <length ; index++) {
+                    System.out.print(buff[index]);
                 }
             }
-        }
-        catch (IOException ioException) {
-            System.out.println(ioException.getClass().getSimpleName() + " _ " + ioException.getMessage());
-        }
-        finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            }
-            catch (IOException ioException) {
-                System.out.println(ioException.getClass().getSimpleName() + " _ " + ioException.getMessage());
+        }catch (IOException exception){
+            System.out.println(exception.getClass().getSimpleName()+" _ "+exception.getMessage());
+        }finally {
+            try{
+                if(reader!=null)
+                 reader.close();
+            }catch (IOException exception){
+                System.out.println(exception.getClass().getSimpleName()+" _ "+exception.getMessage());
             }
         }
     }
-
 }
