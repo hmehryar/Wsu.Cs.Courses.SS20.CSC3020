f86cb33aa26c3ec5973c9ba4e3f769a1e1c071b4
Manraj23s <gg6997@wayne.edu>
2020-07-02 21:28:26 -0400

    CSC3020: gg6997: Lecture18Source

---
+++ src/com/csc3020/lecture18/gg6997/Helper.java
@@ -0,0 +1,14 @@
+package lecture18.gg6997;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Helper {
+    static FileReader openReader(String filePath) throws FileNotFoundException {
+        return  new FileReader(filePath);
+    }
+    static FileWriter openWriter(String filePath) throws IOException {
+        return new FileWriter(filePath);
+    }
+}
