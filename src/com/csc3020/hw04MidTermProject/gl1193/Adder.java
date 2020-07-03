926bf52c6a228ee37af59ddc87d73548538332ed
edgarefigueroa <gl1193@wayne.edu>
2020-07-03 00:00:02 -0400

    CSC3020: gl1193: Lecture18Source

---
+++ src/com/csc3020/hw04MidTermProject/gl1193/Adder.java
@@ -0,0 +1,12 @@
+package com.csc3020.hw04MidTermProject.gl1193;
+
+public class Adder extends CalculateBase {
+
+    public Adder(double leftVal, double rightVal) {
+        super(leftVal, rightVal);
+    }
+
+    public double calculate(){
+        return this.result=this.leftVal+rightVal;
+    }
+}
---
+++ src/com/csc3020/lecture18/gl1193/MyAutoClosable.java
@@ -0,0 +1,27 @@
+package com.csc3020.lecture18.gl1193;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MyAutoClosable implements AutoCloseable {
+    public void saySomething() throws IOException {
+//        FileReader reader = new FileReader("filePath.txt");
+        throw new IOException("Custom Exception");
+//        throw new Exception("New Exception Message");
+        //        System.out.println("Something");
+    }
+
+    public void ReadeFromStreamByFileName() throws FileNotFoundException {
+
+    }
+
+    public void ReadFromStreamByUrl() throws FileNotFoundException {
+
+    }
+    @Override
+    public void close() throws Exception {
+        throw new FileNotFoundException("Custom FileNotFoundException from close method");
+//        System.out.println("close");
+    }
+}
