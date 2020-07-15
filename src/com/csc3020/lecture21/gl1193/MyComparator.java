fc009f29175ebd3376f2c54ad543f90a1559001b
edgarefigueroa <gl1193@wayne.edu>
2020-07-14 23:00:04 -0400

    CSC3020: gl1193: Lecture21Source

---
+++ src/com/csc3020/lecture21/gl1193/MyComparator.java
@@ -0,0 +1,11 @@
+package com.csc3020.lecture21.gl1193;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<MyClass> {
+    @Override
+    public int compare(MyClass o1, MyClass o2) {
+        return o1.getLabel().compareToIgnoreCase(o2.getLabel());
+    }
+
+}
