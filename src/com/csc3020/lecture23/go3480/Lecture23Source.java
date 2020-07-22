ae177ff7b41c9ec748377b5861fdf24083bc0070
arifh-12 <arifh167@gmail.com>
2020-07-22 03:17:21 -0400

    CSC3020: go3480: Lecture23Source

---
+++ src/com/csc3020/lecture23/go3480/Lecture23Source.java
@@ -0,0 +1,16 @@
+package csc3020.lecture23.go3480;
+
+public class Lecture23Source {
+    public static void main(String[] args) {
+
+        IStudent[] oStudent = {
+                new Student(1),
+                new Student(2)
+        };
+
+        IPerson[] oPeople = {
+                new Student(1)
+        };
+
+    }
+}
