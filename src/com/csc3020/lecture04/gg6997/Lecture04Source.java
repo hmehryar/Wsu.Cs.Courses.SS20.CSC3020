95e1586bb700e68d5b37b5b3e71b807e220a6f44
Manraj23s <gg6997@wayne.edu>
2020-07-21 22:32:55 -0400

    CSC3020: gg6997: Lecture04Source

--- src/com/csc3020/lecture04/gg6997/Lecture04Source.java
+++ src/com/csc3020/lecture04/gg6997/Lecture04Source.java
@@ -1,6 +1,6 @@
-package com.csc3020.lecture04.gg6997;
+package csc3020.lecture04.gg6997;
 
-public class Lecture04Source01 {
+public class Lecture04Source {
     public static void main(String[] args){
         System.out.println("gg6997: Manraj Singh");
     }
