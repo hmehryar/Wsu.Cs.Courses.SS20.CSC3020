a6254164808685010e6703700c6cd21b067c7cba
nrtattrie <64509108+nrtattrie@users.noreply.github.com>
2020-07-21 20:52:54 -0400

    CSC3020:he7891:Hw06

---
+++ src/com/csc3020/hw06/he7891/InvalidStatementException.java
@@ -0,0 +1,23 @@
+package com.csc3020.hw06.he7891;
+/*
+ * Author: Nathan Tattrie
+ * Student ID: he7891
+ * Homework Assignment 06
+ * InvalidStatementException.java
+ * */
+
+// Required Items Step 2
+public class InvalidStatementException extends Exception {
+
+    // Required Items Step 2.1
+    public InvalidStatementException(String reason, String statement) {
+        super(reason + ": " + statement);
+    }
+
+    // Required Items Step 2.2
+    public InvalidStatementException (String reason, String statement, Throwable cause) {
+        super(reason + ": " + statement, cause);
+    }
+
+
+}
