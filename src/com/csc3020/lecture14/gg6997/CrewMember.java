cd414d4b6068ede44630bbc44b411f082e94c8e4
Manraj23s <gg6997@wayne.edu>
2020-07-21 22:12:48 -0400

    CSC3020: gg6997: Lecture14Source

--- src/com/csc3020/lecture14/gg6997/CrewMember.java
+++ src/com/csc3020/lecture14/gg6997/CrewMember.java
@@ -1,6 +1,6 @@
 package csc3020.lecture14.gg6997;
 
-import lecture12.gg6997.FlightCrewJob;
+import csc3020.lecture12.gg6997.FlightCrewJob;
 
 public class CrewMember extends csc3020.lecture14.gg6997.Person{
     private FlightCrewJob job;
