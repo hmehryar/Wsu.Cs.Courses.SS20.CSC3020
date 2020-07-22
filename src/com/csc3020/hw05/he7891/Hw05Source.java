ef16573c673f5cca216e22197abb3934715a1496
nrtattrie <64509108+nrtattrie@users.noreply.github.com>
2020-07-21 20:52:24 -0400

    CSC3020:he7891:Hw05

---
+++ src/com/csc3020/hw05/he7891/Hw05Source.java
@@ -0,0 +1,30 @@
+package com.csc3020.hw05.he7891;
+/*
+ * Author: Nathan Tattrie
+ * Student ID: he7891
+ * Homework Assignment 05
+ * Hw05Source.java
+ * */
+
+
+public class Hw05Source {
+
+    public static void main(String[] args) {
+
+        // Required Item Step 1
+        String[] arr = new String[] {
+                "divide 100.0 50.0",
+                "add 25.0 92.0",
+                "subtract 225.0 17.0",
+                "multiply 11.0 3.0"
+        };
+
+        CalculateHelper oCalculateHelper = new CalculateHelper();
+
+        // Required Item Step 8
+        for (int i = 0; i < arr.length; i++) {
+            oCalculateHelper.process(arr[i]);
+            System.out.println(oCalculateHelper.toString());
+        }
+    }
+}
