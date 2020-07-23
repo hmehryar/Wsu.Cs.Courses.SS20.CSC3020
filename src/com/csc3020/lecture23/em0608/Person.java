e50c54c4a8b976ba080cc5f44dde59475ad19eb7
maiselkelly <maiselkelly@gmail.com>
2020-07-23 11:00:53 -0400

    lecture23source.em0608

---
+++ src/com/csc3020/lecture23/em0608/Person.java
@@ -0,0 +1,13 @@
+package com.csc3020.lecture23.em0608;
+
+public abstract class Person implements IPerson{
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
