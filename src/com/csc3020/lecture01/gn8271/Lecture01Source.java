59de2f4a12adee512d2bc32db7fd19892eb675c4
Raisa Zaman <49289864+raisazaman02@users.noreply.github.com>
2020-07-23 20:37:53 -0400

    CSC3020: gn8271: Lecture01Source

---
+++ src/com/csc3020/lecture01/gn8271/Lecture01Source.java
@@ -0,0 +1,18 @@
+package csc3020.lecture01.gn8271;
+// *
+// Author: Raisa Zaman
+// Student ID: gn8271
+// Lecture01
+// *
+public class Lecture01Source {
+    /*
+    Sample Block Comment
+    This code implement the main functionality of the program
+    * */
+    public static void main(String[] args) {
+        //Line Comment
+        // write your code here
+        //int i=0;
+        System.out.println(     "Hello Computer Science 3020: Spring/Summer 2020"   );
+    }
+}
