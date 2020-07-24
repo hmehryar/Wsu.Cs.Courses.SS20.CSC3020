86cd33a013f9278c16e52e9beeb84a9e9e037d2e
Raisa Zaman <49289864+raisazaman02@users.noreply.github.com>
2020-07-24 10:03:00 -0400

    CSC3020: gn8271: Hw03Source

--- src/com/csc3020/hw03/gn8271/Hw03Source.java
+++ src/com/csc3020/hw03/gn8271/Hw03Source.java
@@ -1,4 +1,4 @@
-package hw03.gn8271;
+package csc3020.hw03.gn8271;
 // Homework03
 // Author: Raisa Zaman
 public class Hw03Source {
@@ -13,7 +13,7 @@ public class Hw03Source {
         MathEquation addition = new MathEquation(25,92,'a');
         addition.execute();
         System.out.println("result= "+ addition.getResult());
-        MathEquation subtraction = new MathEquation(225,17,'s');
+       MathEquation subtraction = new MathEquation(225,17,'s');
         subtraction.execute();
         System.out.println("result= "+ subtraction.getResult());
         MathEquation multiplication = new MathEquation(11,3,'m');
